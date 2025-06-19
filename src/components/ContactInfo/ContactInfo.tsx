23661940b09c789a1e16027b6eb878d44cf48362
Aaron Detre <aarondetre@gmail.com>
2025-06-19 13:17:55 -0700

    Contact Info Component

--- src/components/ContactInfo/ContactInfo.tsx
+++ src/components/ContactInfo/ContactInfo.tsx
@@ -1,11 +1,21 @@
-import React, { FC } from 'react';
-import styles from './ContactInfo.module.css';
+import React, { FC } from "react";
+import styles from "./ContactInfo.module.css";
+import { FaInstagram } from "react-icons/fa";
+import { MdOutlineEmail } from "react-icons/md";
 
 interface ContactInfoProps {}
 
 const ContactInfo: FC<ContactInfoProps> = () => (
   <div className={styles.ContactInfo} data-testid="ContactInfo">
-    ContactInfo Component
+    <a
+      className="instagram"
+      href="https://www.instagram.com/aaronscameraphotos/"
+    >
+      <FaInstagram />
+    </a>
+    <a className="email" href="mailto:aarondetre@gmail.com">
+      <MdOutlineEmail />
+    </a>
   </div>
 );
 
