1478d7b0a006ed13cb81617619f42e765138027b
Aaron Detre <aarondetre@gmail.com>
2025-08-22 17:31:45 -0700

    Finished carousel functionality and styling

--- src/components/CarouselView/CarouselView.tsx
+++ src/components/CarouselView/CarouselView.tsx
@@ -2,6 +2,7 @@ import React, { FC } from "react";
 import styles from "./CarouselView.module.css";
 import { IoIosArrowBack } from "react-icons/io";
 import { IoIosArrowForward } from "react-icons/io";
+import { RxCross2 } from "react-icons/rx";
 import photoOrdering from "../../photoOrdering.json";
 
 interface CarouselViewProps {
@@ -10,36 +11,71 @@ interface CarouselViewProps {
 }
 
 const CarouselView: FC<CarouselViewProps> = ({ currentPhoto, setCarousel }) => {
+  const currentPhotoIndex = findPhotoIndex(currentPhoto);
+
+  const isLeftArrowActive = currentPhotoIndex !== 0;
+  const isRightArrowActive = currentPhotoIndex !== photoOrdering.length - 1;
+
   return (
     <div className={styles.CarouselView}>
-      <IoIosArrowBack
-        onClick={() => setCarousel(changePhoto(currentPhoto, -1))}
+      <RxCross2
+        className={styles.exit}
+        onClick={() => updateCarousel(setCarousel, "")}
       />
-      <img
-        src={`/pictures/${currentPhoto}`}
-        width={window.screen.width * 0.9}
-        onClick={() => setCarousel("")}
-      />
-      <IoIosArrowForward
-        onClick={() => setCarousel(changePhoto(currentPhoto, 1))}
-      />
+      <div className={styles.flexContainer}>
+        <IoIosArrowBack
+          className={getArrowClassNames(isLeftArrowActive, true)}
+          onClick={() => {
+            if (isLeftArrowActive) {
+              updateCarousel(setCarousel, changePhoto(currentPhotoIndex, -1));
+            }
+          }}
+        />
+        <img
+          src={`/pictures/${currentPhoto}`}
+          style={{
+            maxWidth: window.innerWidth * 0.85,
+            maxHeight: window.innerHeight * 0.9,
+          }}
+        />
+        <IoIosArrowForward
+          className={getArrowClassNames(isRightArrowActive, false)}
+          onClick={() => {
+            if (isRightArrowActive) {
+              updateCarousel(setCarousel, changePhoto(currentPhotoIndex, 1));
+            }
+          }}
+        />
+      </div>
     </div>
   );
 };
 
-function changePhoto(currentPhoto: string, indexChange: number): string {
-  const currentIndex = photoOrdering.findIndex(
-    (photo) => photo.name === currentPhoto
-  );
-  console.assert(currentIndex !== -1);
-  console.assert(currentIndex !== 0 || indexChange !== -1);
+function findPhotoIndex(photoName: string): number {
+  return photoOrdering.findIndex((photo) => photo.name === photoName);
+}
+
+function changePhoto(currentPhotoIndex: number, indexChange: number): string {
+  console.assert(currentPhotoIndex !== -1);
+  console.assert(currentPhotoIndex !== 0 || indexChange !== -1);
   console.assert(
-    currentIndex !== photoOrdering.length - 1 || indexChange !== 1
+    currentPhotoIndex !== photoOrdering.length - 1 || indexChange !== 1
   );
 
-  const nextPhoto = photoOrdering.at(currentIndex + indexChange)?.name ?? "";
+  const nextPhoto =
+    photoOrdering.at(currentPhotoIndex + indexChange)?.name ?? "";
   console.log(nextPhoto);
   return nextPhoto;
+}
+
+function updateCarousel(setCarousel: Function, value: string): void {
+  setCarousel(value);
+}
+
+function getArrowClassNames(isArrowActive: boolean, isLeft: boolean): string {
+  return `${styles.arrow} ${
+    isArrowActive ? styles.arrowActive : styles.arrowInactive
+  } ${isLeft ? styles.leftArrow : styles.rightArrow}`;
 }
 
 export default CarouselView;
